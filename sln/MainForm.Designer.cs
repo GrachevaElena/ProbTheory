a561226186bc047ae436056d4b03cefc4b663c9d
Alyona <alyona-gra98@yandex.ru>
2018-03-21 13:10:19 +0300

    part 1

---
+++ sln/MainForm.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace ProbTheory
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBoxLambda = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.labelNumExperiments = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.table = new System.Windows.Forms.DataGridView();
+            this.buttonExperiment = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(-2, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(710, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " С.в. η — время обслуживания покупателя в кассе магазина; распределена показатель" +
+    "но с параметром λ.";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBoxLambda);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(9, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(248, 464);
+            this.panel1.TabIndex = 1;
+            // 
+            // textBoxLambda
+            // 
+            this.textBoxLambda.Location = new System.Drawing.Point(46, 168);
+            this.textBoxLambda.Name = "textBoxLambda";
+            this.textBoxLambda.Size = new System.Drawing.Size(116, 20);
+            this.textBoxLambda.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(15, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "λ=";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(11, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(225, 102);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(263, 40);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(491, 464);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.buttonReset);
+            this.tabPage1.Controls.Add(this.labelNumExperiments);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.table);
+            this.tabPage1.Controls.Add(this.buttonExperiment);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(483, 438);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Моделирование";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // labelNumExperiments
+            // 
+            this.labelNumExperiments.AutoSize = true;
+            this.labelNumExperiments.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelNumExperiments.Location = new System.Drawing.Point(305, 359);
+            this.labelNumExperiments.Name = "labelNumExperiments";
+            this.labelNumExperiments.Size = new System.Drawing.Size(16, 17);
+            this.labelNumExperiments.TabIndex = 3;
+            this.labelNumExperiments.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(10, 359);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(289, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Количество проведенных экспериментов: ";
+            // 
+            // table
+            // 
+            this.table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table.Location = new System.Drawing.Point(13, 13);
+            this.table.Name = "table";
+            this.table.Size = new System.Drawing.Size(454, 333);
+            this.table.TabIndex = 1;
+            // 
+            // buttonExperiment
+            // 
+            this.buttonExperiment.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonExperiment.Location = new System.Drawing.Point(93, 387);
+            this.buttonExperiment.Name = "buttonExperiment";
+            this.buttonExperiment.Size = new System.Drawing.Size(187, 45);
+            this.buttonExperiment.TabIndex = 0;
+            this.buttonExperiment.Text = "Провести эксперимент";
+            this.buttonExperiment.UseVisualStyleBackColor = true;
+            this.buttonExperiment.Click += new System.EventHandler(this.buttonExperiment_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonReset.Location = new System.Drawing.Point(287, 387);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(187, 45);
+            this.buttonReset.TabIndex = 4;
+            this.buttonReset.Text = "Сбросить результаты";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 516);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Моделирование случайных величин";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TextBox textBoxLambda;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonExperiment;
+        private System.Windows.Forms.DataGridView table;
+        private System.Windows.Forms.Label labelNumExperiments;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonReset;
+    }
+}
---
+++ sln/MainForm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProbTheory
+{
+    public partial class MainForm : Form
+    {
+        double Lambda = 1;
+        List<double> listRes = new List<double>();
+        Random rnd = new Random();
+
+        const int nCol = 4;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            textBoxLambda.Text = "1";
+            for (int i = 0; i < nCol; i++)
+                table.Columns.Add("column" + i.ToString(), " ");
+        }
+
+        private bool CheckLambda()
+        {
+            bool res = Double.TryParse(textBoxLambda.Text, out Lambda);
+            if (!res || Lambda <= 0)
+            {
+                MessageBox.Show("Неверное значение λ");
+                return false;
+            }
+            return true;
+        }
+
+        public double MakeExperiment()
+        {
+            double u = rnd.NextDouble();
+            return -1.0/Lambda*Math.Log(u);
+        }
+
+        private void AddRowToTable()
+        {
+            table.Rows.Add();
+        }
+
+        private void ClearTable()
+        {
+            table.Rows.Clear();
+        }
+
+        private void FillTable()
+        {
+            int nRow = table.Rows.Count;
+            if (listRes.Count / nCol + (listRes.Count % nCol == 0 ? 0 : 1) > nRow)
+                AddRowToTable();
+            for (int i = 0; i < table.Rows.Count; i++)
+                for (int j = 0; j < table.Columns.Count; j++)
+                    table[j, i].Value = "";
+            for (int i=0; i<listRes.Count; i++)
+            {
+                int col = i - (i / nCol) * nCol;
+                int row = (i + 1) / nCol + ((i + 1) % nCol == 0 ? 0 : 1) - 1;
+                table[col, row].Value = listRes[i].ToString();
+            }
+        }
+
+        private void ClearRes()
+        {
+            listRes.Clear();
+            labelNumExperiments.Text = "0";
+        }
+
+        private void buttonExperiment_Click(object sender, EventArgs e)
+        {
+            if (!CheckLambda()) return;
+            double res=MakeExperiment();
+            listRes.Add(res);
+            listRes.Sort();
+            labelNumExperiments.Text = listRes.Count.ToString();
+            FillTable();
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            ClearTable();
+            ClearRes();
+        }
+    }
+}
